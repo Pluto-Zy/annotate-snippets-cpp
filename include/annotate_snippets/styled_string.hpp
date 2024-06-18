a6a9aa1e6c60d901d0067cf9c8379771842b7508
ZPlutoY <38772646+Pluto-Zy@users.noreply.github.com>
2024-06-19 00:55:13 +0800

    (feat) Implement `StyledString` class.
    - Introduce `StyledString` class, inheriting from `detail::StyledStringImpl`, to own the underlying string and support appending styled strings at the end.
    - Design `StyledString` to serve as a rendering target within the renderer.

---
+++ include/annotate_snippets/styled_string.hpp
@@ -0,0 +1,198 @@
+#ifndef ANNOTATE_SNIPPETS_STYLED_STRING_HPP
+#define ANNOTATE_SNIPPETS_STYLED_STRING_HPP
+
+#include "annotate_snippets/detail/styled_string_impl.hpp"
+#include "annotate_snippets/style.hpp"
+#include "annotate_snippets/styled_string_view.hpp"
+
+#include <algorithm>
+#include <cstddef>
+#include <string>
+#include <string_view>
+#include <vector>
+
+namespace ants {
+class StyledString : public detail::StyledStringImpl {
+    using Base = detail::StyledStringImpl;
+
+public:
+    /// Constructs an empty `StyledString`.
+    StyledString() = default;
+
+    /// Constructs a `StyledString` whose content is `content` and the style of the whole string is
+    /// `style`.
+    StyledString(std::string content, Style style) :
+        Base(content.size(), style), content_(std::move(content)) { }
+
+    /// Constructs a `StyledString` whose content is `content` and the style of the whole string is
+    /// `style`. This function has the same effect as the corresponding constructor.
+    static auto styled(std::string content, Style style) -> StyledString {
+        return { std::move(content), style };
+    }
+
+    /// Constructs a `StyledString` whose content is `content` and the style of the whole string
+    /// will be inferred from the context in which the string is used (i.e. the `Style::Auto`
+    /// style).
+    static auto inferred(std::string content) -> StyledString {
+        return styled(std::move(content), Style::Auto);
+    }
+
+    /// Constructs a `StyledString` whose content is `content` with no style (i.e. the
+    /// `Style::Default` style). It will be rendered as the default style of the output environment.
+    static auto plain(std::string content) -> StyledString {
+        return styled(std::move(content), Style::Default);
+    }
+
+    auto content() const -> std::string const& {
+        return content_;
+    }
+
+    auto content() -> std::string& {
+        return content_;
+    }
+
+    auto empty() const -> bool {
+        return content_.empty();
+    }
+
+    using Base::set_style;
+
+    /// Sets the style of the substring starting at `start_index` and ending at the end of the whole
+    /// string. Any existing styles for the characters in this substring will be overwritten.
+    void set_style(Style style, std::size_t start_index) {
+        set_style(style, start_index, content_.size());
+    }
+
+    /// Sets the style of the whole string.
+    void set_style(Style style) {
+        // clang-format off
+        std::vector<StyledPart> {
+            { .start_index = 0, .style = style },
+            { .start_index = content_.size(), .style {} },
+        }
+            .swap(styled_parts_);
+        // clang-format on
+    }
+
+    auto with_style(
+        Style style,
+        std::size_t start_index,
+        std::size_t end_index
+    ) & -> StyledString& {
+        set_style(style, start_index, end_index);
+        return *this;
+    }
+
+    auto with_style(
+        Style style,
+        std::size_t start_index,
+        std::size_t end_index
+    ) && -> StyledString&& {
+        set_style(style, start_index, end_index);
+        return std::move(*this);
+    }
+
+    auto with_style(Style style, std::size_t start_index) & -> StyledString& {
+        set_style(style, start_index);
+        return *this;
+    }
+
+    auto with_style(Style style, std::size_t start_index) && -> StyledString&& {
+        set_style(style, start_index);
+        return std::move(*this);
+    }
+
+    auto with_style(Style style) & -> StyledString& {
+        set_style(style);
+        return *this;
+    }
+
+    auto with_style(Style style) && -> StyledString&& {
+        set_style(style);
+        return std::move(*this);
+    }
+
+    /// Appends the string `content` to the end of the current `StyledString` with the specified
+    /// `style`. Existing parts of the `StyledString` remain unaffected.
+    void append(std::string_view content, Style style) {
+        if (content_.empty()) {
+            // If the original string is empty, `styled_parts_` contains two elements with
+            // `start_index` both set to 0; we need to remove one of them.
+            styled_parts_.resize(1);
+        }
+
+        styled_parts_.back().style = style;
+        content_.append(content);
+
+        styled_parts_.push_back(StyledPart { .start_index = content_.size(), .style {} });
+    }
+
+    /// Appends the string `content` to the end of the current `StyledString` with the specified
+    /// `style`. If `style` is `Style::Auto`, it is changed to `auto_replacement`. Existing parts of
+    /// the `StyledString` remain unaffected.
+    void append(std::string_view content, Style style, Style auto_replacement) {
+        append(content, style.is_auto_style() ? auto_replacement : style);
+    }
+
+    /// Appends the sequence of styled strings specified by `parts` to the end of the current
+    /// `StyledString`. This method is typically used to add a `StyledStringView` or `StyledString`
+    /// line by line to the current string.
+    void append(std::vector<StyledStringViewPart> const& parts) {
+        for (StyledStringViewPart const& part : parts) {
+            append(part.content, part.style);
+        }
+    }
+
+    /// Appends the sequence of styled strings specified by `parts` to the end of the current
+    /// `StyledString`. If the style of any part is `Style::Auto`, it is replaced with
+    /// `auto_replacement`. This method is typically used to add a `StyledStringView` or
+    /// `StyledString` line by line to the current string.
+    void append(std::vector<StyledStringViewPart> const& parts, Style auto_replacement) {
+        for (StyledStringViewPart const& part : parts) {
+            append(part.content, part.style, auto_replacement);
+        }
+    }
+
+    /// Appends a newline character at the end of the string to ensure that subsequent additions
+    /// begin on a new line.
+    void append_newline() {
+        content_.push_back('\n');
+        // We simply set the style of the newline character to match the style of the character
+        // before it, as the style of the newline character is not observable in the final
+        // rendering. In other words, this approach does not affect the results of
+        // `styled_line_parts()`.
+        styled_parts_.back().start_index = content_.size();
+    }
+
+    /// Splits `content` into several `StyledStringPart`s by line and style, and puts substrings
+    /// consisting of consecutive characters of the same style into one `StyledStringPart`. If there
+    /// are multiple lines in a substring, splits each line into a separate `StyledStringPart`.
+    ///
+    /// Example:
+    ///
+    ///     StyledString::inferred("Hello\nWorld")
+    ///         .with_style(Style::Highlight, 2, 8)
+    ///         .styled_line_parts()
+    ///
+    ///     returns
+    ///     {
+    ///         { { "He", Style::Auto }, { "llo", Style::Highlight } }  // First line
+    ///         { { "Wo", Style::Highlight }, { "rld", Style::Auto } }  // Second line
+    ///     }
+    ///
+    /// Note that the returned `StyledStringViewPart` does not take ownership of the underlying
+    /// string. Once the `StyledString` is destroyed, the returned value becomes invalid.
+    ///
+    /// @return The first level of the return value array represents the lines in `content`, and the
+    /// second level saves the `StyledStringPart`s consisting of consecutive characters of the same
+    /// style contained in the same line.
+    auto styled_line_parts() const -> std::vector<std::vector<StyledStringViewPart>> {
+        return Base::styled_line_parts(content_);
+    }
+
+private:
+    std::string content_;
+};
+}  // namespace ants
+
+#endif  // ANNOTATE_SNIPPETS_STYLED_STRING_HPP
---
+++ test/styled_string_test.cpp
@@ -0,0 +1,345 @@
+#include "annotate_snippets/style.hpp"
+#include "annotate_snippets/styled_string.hpp"
+
+#include "gtest/gtest.h"
+
+namespace {
+using LineParts = std::vector<std::vector<ants::StyledStringViewPart>>;
+
+TEST(StyledStringTest, AppendContent) {
+    {
+        auto str = ants::StyledString::inferred("Hello");
+        EXPECT_EQ(
+            str.styled_line_parts(),
+            (LineParts { { { .content = "Hello", .style = ants::Style::Auto } } })
+        );
+        str.append("World", ants::Style::Auto);
+        EXPECT_EQ(str.content(), "HelloWorld");
+        EXPECT_EQ(
+            str.styled_line_parts(),
+            (LineParts { { { .content = "HelloWorld", .style = ants::Style::Auto } } })
+        );
+        str.append(".", ants::Style::Default);
+        EXPECT_EQ(str.content(), "HelloWorld.");
+        // clang-format off
+        EXPECT_EQ(
+            str.styled_line_parts(),
+            (LineParts { {
+                { .content = "HelloWorld", .style = ants::Style::Auto },
+                { .content = ".", .style = ants::Style::Default },
+            } })
+        );
+        // clang-format on
+    }
+
+    {
+        auto str = ants::StyledString();
+        str.append("Hello", ants::Style::Auto);
+        EXPECT_EQ(str.content(), "Hello");
+        EXPECT_EQ(
+            str.styled_line_parts(),
+            (LineParts { { { .content = "Hello", .style = ants::Style::Auto } } })
+        );
+    }
+
+    {
+        auto str = ants::StyledString();
+        str.append("", ants::Style::Auto);
+        EXPECT_EQ(str.content(), "");
+        EXPECT_TRUE(str.empty());
+        EXPECT_EQ(str.styled_line_parts(), LineParts {});
+    }
+
+    {
+        auto str = ants::StyledString();
+        str.append("", ants::Style::Auto);
+        str.append("", ants::Style::Default);
+        str.append("", ants::Style::Highlight);
+        EXPECT_EQ(str.content(), "");
+        EXPECT_TRUE(str.empty());
+        EXPECT_EQ(str.styled_line_parts(), LineParts {});
+        str.append("Hello", ants::Style::Addition);
+        EXPECT_EQ(str.content(), "Hello");
+        EXPECT_EQ(
+            str.styled_line_parts(),
+            (LineParts { { { .content = "Hello", .style = ants::Style::Addition } } })
+        );
+    }
+
+    {
+        auto str = ants::StyledString::inferred("Hello").with_style(ants::Style::Default, 2);
+        str.append("World", ants::Style::Auto);
+        EXPECT_EQ(str.content(), "HelloWorld");
+        // clang-format off
+        EXPECT_EQ(
+            str.styled_line_parts(),
+            (LineParts { {
+                { .content = "He", .style = ants::Style::Auto },
+                { .content = "llo", .style = ants::Style::Default },
+                { .content = "World", .style = ants::Style::Auto },
+            } })
+        );
+        // clang-format on
+    }
+
+    {
+        auto str = ants::StyledString::inferred("Hello").with_style(ants::Style::Default, 2);
+        str.append("World", ants::Style::Default);
+        EXPECT_EQ(str.content(), "HelloWorld");
+        // clang-format off
+        EXPECT_EQ(
+            str.styled_line_parts(),
+            (LineParts { {
+                { .content = "He", .style = ants::Style::Auto },
+                { .content = "lloWorld", .style = ants::Style::Default },
+            } })
+        );
+        // clang-format on
+    }
+
+    {
+        auto str = ants::StyledString::plain("Hello");
+        str.append("World", ants::Style::Auto, ants::Style::Highlight);
+        EXPECT_EQ(str.content(), "HelloWorld");
+        // clang-format off
+        EXPECT_EQ(
+            str.styled_line_parts(),
+            (LineParts { {
+                { .content = "Hello", .style = ants::Style::Default },
+                { .content = "World", .style = ants::Style::Highlight },
+            } })
+        );
+        // clang-format on
+    }
+
+    {
+        auto str = ants::StyledString::plain("Hello");
+        str.append("World", ants::Style::Auto, ants::Style::Default);
+        EXPECT_EQ(str.content(), "HelloWorld");
+        EXPECT_EQ(
+            str.styled_line_parts(),
+            (LineParts { { { .content = "HelloWorld", .style = ants::Style::Default } } })
+        );
+    }
+
+    {
+        auto str = ants::StyledString::inferred("Hello");
+        str.append("World", ants::Style::Auto, ants::Style::Highlight);
+        EXPECT_EQ(str.content(), "HelloWorld");
+        // clang-format off
+        EXPECT_EQ(
+            str.styled_line_parts(),
+            (LineParts { {
+                { .content = "Hello", .style = ants::Style::Auto },
+                { .content = "World", .style = ants::Style::Highlight },
+            } })
+        );
+        // clang-format on
+    }
+
+    {
+        auto str = ants::StyledString::inferred("Hello");
+        str.append("World", ants::Style::Auto, ants::Style::Highlight);
+        EXPECT_EQ(str.content(), "HelloWorld");
+        str.set_style(ants::Style::Default, 3, 6);
+        // clang-format off
+        EXPECT_EQ(
+            str.styled_line_parts(),
+            (LineParts { {
+                { .content = "Hel", .style = ants::Style::Auto },
+                { .content = "loW", .style = ants::Style::Default },
+                { .content = "orld", .style = ants::Style::Highlight },
+            } })
+        );
+        // clang-format on
+    }
+}
+
+TEST(StyledStringTest, AppendParts) {
+    {
+        auto str = ants::StyledString::inferred("Hello");
+        auto const append = ants::StyledString::styled("World", ants::Style::Addition);
+        str.append(append.styled_line_parts().front());
+        EXPECT_EQ(str.content(), "HelloWorld");
+        // clang-format off
+        EXPECT_EQ(
+            str.styled_line_parts(),
+            (LineParts { {
+                { .content = "Hello", .style = ants::Style::Auto },
+                { .content = "World", .style = ants::Style::Addition },
+            } })
+        );
+        // clang-format on
+    }
+
+    {
+        auto str = ants::StyledString::inferred("Hello");
+        auto const append = ants::StyledString::inferred("World");
+        str.append(append.styled_line_parts().front());
+        EXPECT_EQ(str.content(), "HelloWorld");
+        EXPECT_EQ(
+            str.styled_line_parts(),
+            (LineParts { { { .content = "HelloWorld", .style = ants::Style::Auto } } })
+        );
+    }
+
+    {
+        auto str = ants::StyledString::inferred("Hello");
+        auto const append =
+            ants::StyledString::inferred("World").with_style(ants::Style::Default, 3);
+        str.append(append.styled_line_parts().front());
+        EXPECT_EQ(str.content(), "HelloWorld");
+        // clang-format off
+        EXPECT_EQ(
+            str.styled_line_parts(),
+            (LineParts { {
+                { .content = "HelloWor", .style = ants::Style::Auto },
+                { .content = "ld", .style = ants::Style::Default },
+            } })
+        );
+        // clang-format on
+    }
+
+    {
+        auto str = ants::StyledString::inferred("Hello");
+        auto const append = ants::StyledString::inferred("World")
+                                .with_style(ants::Style::custom(1), 0)
+                                .with_style(ants::Style::custom(2), 1)
+                                .with_style(ants::Style::custom(3), 3)
+                                .with_style(ants::Style::custom(4), 4);
+        str.append(append.styled_line_parts().front());
+        EXPECT_EQ(str.content(), "HelloWorld");
+        // clang-format off
+        EXPECT_EQ(
+            str.styled_line_parts(),
+            (LineParts { {
+                { .content = "Hello", .style = ants::Style::Auto },
+                { .content = "W", .style = ants::Style::custom(1) },
+                { .content = "or", .style = ants::Style::custom(2) },
+                { .content = "l", .style = ants::Style::custom(3) },
+                { .content = "d", .style = ants::Style::custom(4) },
+            } })
+        );
+        // clang-format on
+    }
+
+    {
+        auto str = ants::StyledString::inferred("");
+        auto const append = ants::StyledString::inferred("Hello")
+                                .with_style(ants::Style::custom(1), 0)
+                                .with_style(ants::Style::custom(2), 1)
+                                .with_style(ants::Style::custom(3), 3)
+                                .with_style(ants::Style::custom(4), 4);
+        str.append(append.styled_line_parts().front());
+        EXPECT_EQ(str.content(), "Hello");
+        EXPECT_EQ(str.styled_line_parts(), append.styled_line_parts());
+    }
+
+    {
+        auto str = ants::StyledString::inferred("Hello");
+        auto const append =
+            ants::StyledString::inferred("World").with_style(ants::Style::Default, 3);
+        str.append(append.styled_line_parts().front(), ants::Style::Highlight);
+        EXPECT_EQ(str.content(), "HelloWorld");
+        // clang-format off
+        EXPECT_EQ(
+            str.styled_line_parts(),
+            (LineParts { {
+                { .content = "Hello", .style = ants::Style::Auto },
+                { .content = "Wor", .style = ants::Style::Highlight },
+                { .content = "ld", .style = ants::Style::Default },
+            } })
+        );
+        // clang-format on
+    }
+
+    {
+        auto str = ants::StyledString::plain("Hello");
+        auto const append =
+            ants::StyledString::inferred("World").with_style(ants::Style::Highlight, 3);
+        str.append(append.styled_line_parts().front(), ants::Style::Default);
+        EXPECT_EQ(str.content(), "HelloWorld");
+        // clang-format off
+        EXPECT_EQ(
+            str.styled_line_parts(),
+            (LineParts { {
+                { .content = "HelloWor", .style = ants::Style::Default },
+                { .content = "ld", .style = ants::Style::Highlight },
+            } })
+        );
+        // clang-format on
+    }
+}
+
+TEST(StyledStringTest, AppendNewline) {
+    {
+        auto str = ants::StyledString::inferred("Hello");
+        str.append_newline();
+        EXPECT_EQ(str.content(), "Hello\n");
+        EXPECT_EQ(
+            str.styled_line_parts(),
+            (LineParts { { { .content = "Hello", .style = ants::Style::Auto } } })
+        );
+
+        str.append_newline();
+        EXPECT_EQ(str.content(), "Hello\n\n");
+        // clang-format off
+        EXPECT_EQ(
+            str.styled_line_parts(),
+            (LineParts { {
+                { { .content = "Hello", .style = ants::Style::Auto } },
+                { { .content = "", .style = ants::Style::Auto } },
+            } })
+        );
+        // clang-format on
+
+        str.append("World", ants::Style::Default);
+        EXPECT_EQ(str.content(), "Hello\n\nWorld");
+        // clang-format off
+        EXPECT_EQ(
+            str.styled_line_parts(),
+            (LineParts { {
+                { { .content = "Hello", .style = ants::Style::Auto } },
+                { { .content = "", .style = ants::Style::Auto } },
+                { { .content = "World", .style = ants::Style::Default } },
+            } })
+        );
+        // clang-format on
+    }
+
+    {
+        auto str = ants::StyledString();
+        str.append_newline();
+        EXPECT_EQ(str.content(), "\n");
+        EXPECT_EQ(
+            str.styled_line_parts(),
+            (LineParts { { { .content = "", .style = ants::Style::Auto } } })
+        );
+
+        str.append("Hello", ants::Style::Default);
+        EXPECT_EQ(str.content(), "\nHello");
+        // clang-format off
+        EXPECT_EQ(
+            str.styled_line_parts(),
+            (LineParts { {
+                { { .content = "", .style = ants::Style::Auto } },
+                { { .content = "Hello", .style = ants::Style::Default } },
+            } })
+        );
+        // clang-format on
+
+        str.append_newline();
+        str.append("World", ants::Style::Highlight);
+        // clang-format off
+        EXPECT_EQ(
+            str.styled_line_parts(),
+            (LineParts { {
+                { { .content = "", .style = ants::Style::Auto } },
+                { { .content = "Hello", .style = ants::Style::Default } },
+                { { .content = "World", .style = ants::Style::Highlight } },
+            } })
+        );
+        // clang-format on
+    }
+}
+}  // namespace
