c69584996b6a0d7bae5758ccf933986e59d44d09
ZPlutoY <38772646+Pluto-Zy@users.noreply.github.com>
2024-06-19 17:16:24 +0800

    (feat) Add `unicode_display_width` library as dependency.
    - Integrate `unicode_display_width` library to calculate the display width of strings in the console.

---
+++ cmake/AddUnicodeDisplayWidth.cmake
@@ -0,0 +1,9 @@
+include(FetchContent)
+
+FetchContent_Declare(
+    unicode_display_width
+    GIT_REPOSITORY https://github.com/p-ranav/unicode_display_width.git
+    GIT_TAG master
+)
+
+FetchContent_MakeAvailable(unicode_display_width)
